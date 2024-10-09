a4c7d5a7a76b3c7441e5954ab3fb1a3ac03e40d8
= <=>
2024-10-09 11:17:18 +0100

    Complete Week 2 tasks, Encapsulation and Inheritance

--- src/Main.kt
+++ src/Main.kt
@@ -1,22 +1,28 @@
 fun main () {
-    val ozzy = Cat("Ozzy", 3, 10)
-    val margo = Cat("Margo", 1, 5)
+//    val ozzy = Cat("Ozzy", 3, 10)
+//    val margo = Cat("Margo", 1, 5)
+//
+//    ozzy.walk(2)
+//    margo.walk(10)
+//    println(ozzy.toString())
+//    println(margo.toString())
+//
+//    for (i in 1..5) {
+//        println(if (ozzy.walk(1)) "Walk successful" else "Walk failed")
+//    }
+//    println(ozzy.toString())
+//
+//    for (i in 1..10) {
+//        println(if (ozzy.eat(2)) "Eat successful" else "Eat failed")
+//    }
+//    println(ozzy.toString())
 
-    ozzy.walk(2)
-    margo.walk(10)
-    println(ozzy.toString())
-    println(margo.toString())
+    val jed = Student(1, "Jed", "D&TS")
+    println("Enter the mark for ${jed.name}: ")
+    val inputMark = readln().toDouble()
+    jed.mark = inputMark
 
-    for (i in 1..5) {
-        println(if (ozzy.walk(1)) "Walk successful" else "Walk failed")
-    }
-    println(ozzy.toString())
-
-    for (i in 1..10) {
-        println(if (ozzy.eat(2)) "Eat successful" else "Eat failed")
-    }
-    println(ozzy.toString())
-
-    val jed = Student(1, "Jed", "D&TS", 90.0)
-
+    println("Mark: ${jed.mark}")
+    println("Grade: ${jed.getGrade()}")
+    println("Passed: ${jed.didPass()}")
 }
--- src/Student.kt
+++ src/Student.kt
@@ -1,17 +1,33 @@
-class Student(val id: Int, val name: String, val course: String, markIn: Double) {
+class Student(val id: Int, val name: String, val course: String) {
 
-    var mark = 0.0
+    var mark = -1.0
         set(newMark) {
             if(newMark in 0.0..100.0) {
                 field = newMark
             }
         }
 
-    init {
-        mark = markIn
-    }
-
     override fun toString() : String {
         return "ID: $id Name: $name Course: $course Mark: $mark"
+    }
+
+    fun getGrade() : String {
+        return if (mark >= 70) {
+            ("First")
+        } else if (mark >= 60) {
+            ("2/1")
+        } else if (mark >= 50) {
+            ("2/2")
+        } else if (mark >= 40) {
+            ("Third")
+        } else if (mark >= 0) {
+            ("Fail")
+        } else {
+            ("Error: Mark is invalid")
+        }
+    }
+
+    fun didPass() : Boolean {
+        return mark >= 40
     }
 }
