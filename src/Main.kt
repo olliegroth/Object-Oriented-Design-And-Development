6e142177d78ed52f57b276fc8554a3d05d8848c3
= <=>
2024-10-09 11:38:38 +0100

    Week 3, Task 1

--- src/Main.kt
+++ src/Main.kt
@@ -17,12 +17,45 @@ fun main () {
 //    }
 //    println(ozzy.toString())
 
-    val jed = Student(1, "Jed", "D&TS")
-    println("Enter the mark for ${jed.name}: ")
-    val inputMark = readln().toDouble()
-    jed.mark = inputMark
+//    val jed = Student(1, "Jed", "D&TS")
+//    println("Enter the mark for ${jed.name}: ")
+//    val inputMark = readln().toDouble()
+//    jed.mark = inputMark
+//
+//    println("Mark: ${jed.mark}")
+//    println("Grade: ${jed.getGrade()}")
+//    println("Passed: ${jed.didPass()}")
 
-    println("Mark: ${jed.mark}")
-    println("Grade: ${jed.getGrade()}")
-    println("Passed: ${jed.didPass()}")
+    val students = mutableListOf<Student>()
+
+    var moreStudents = true
+    var idCounter = 0
+
+    while (moreStudents) {
+        idCounter++
+        println("Enter the name of student $idCounter: ")
+        val name = readln()
+        println("Enter the course for $name: ")
+        val course = readln()
+
+        val student = Student(idCounter, name, course)
+
+        println("Enter the mark for $name: ")
+        val inputMark = readln().toDouble()
+        student.mark = inputMark
+
+        students.add(student)
+
+        println("Do you want to enter another student? (y/n)")
+        val response = readln()
+        if (response.lowercase() != "y") {
+            moreStudents = false
+        }
+    }
+    println("\nStudent List:")
+    for (student in students) {
+        println("ID: ${student.id}, Name: ${student.name}, Course: ${student.course}")
+        println("Mark: ${student.mark}, Grade: ${student.getGrade()}, Passed: ${student.didPass()}")
+        println("--------------------------------")
+    }
 }
